52ffc12dcb72a8e580c33704a0f95d4923882190
LeonardoZambello <leonardo.zambello@hotmail.com>
2020-11-16 12:46:26 -0300

    test: ensure EmailValidatorAdapter calls validator with the correct email

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,7 +1,7 @@
 import { EmailValidator } from '../presentation/protocols/email-validator'
 import validator from 'validator'
 
-export class EmailValidatorAdapter implements EmailValidator {
+export class EmailValdatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
     return validator.isEmail(email)
   }

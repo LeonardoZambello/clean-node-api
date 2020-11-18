6a7216770783ecbecd4ec00fcb4c2e5eb1fc4d3a
LeonardoZambello <leonardo.zambello@hotmail.com>
2020-11-17 21:40:01 -0300

    feat: changes

--- src/infra/db/mongodb/helpers/mongo-helpers.ts
+++ src/infra/db/mongodb/helpers/mongo-helpers.ts
@@ -1,5 +1,4 @@
-import { MongoClient } from 'mongodb'
-import { disconnect } from 'process'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -10,7 +9,12 @@ export const MongoHelper = {
       useUnifiedTopology: true
     })
   },
+
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
